4209ac279c7ffa5ccbbd412e70967dfa07ab5dbe
varunsampann <varunsampann@gmail.com>
2025-01-21 14:01:21 +0530

    project initiated

---
+++ src/main/java/com/varun/bsm/BloodSampleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.varun.bsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodSampleManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodSampleManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varun/bsm/BloodSampleManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.bsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodSampleManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
